4a6dbee560a22bb7f30acdc9ba78dd04944f4b7e
amanda16229 <amanda.olyer@gmail.com>
2025-08-24 11:00:32 -0400

    first use of tkinter

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -1,18 +1,21 @@
+import tkinter as tk
 import requests
-from twilio.rest import Client
+
 
 API_KEY = '<api_key>'
 ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
 
-ACCOUNT_SID = '<sid>'
-AUTH_TOKEN = '<token>'
+window = tk.Tk()
+window.title('Weather Alert App')
+tk.Label(window, text="Tampa FL weather: ").pack() # using tampa latitude & longitude
+
 
 # dictionary - must follow the keys provided in API docs
 weather_params = {
-    "lat": 123,
-    "lon": 123,
+    "lat": 27.9,
+    "lon": 82.4,
     "appid": API_KEY,
-    "cnt": 4,
+    "cnt": 4, # (optional) - only uses next 4 entries of weather data
 }
 
 # want to make our GET requests to the endpoint - check the data they already have, get it
@@ -20,7 +23,6 @@ weather_params = {
 response = requests.get(ENDPOINT, params=weather_params)
 
 weather_data = response.json()
-# print(weather_data['list'][0]['main']['feels_like'])
 
 will_rain = False
 
@@ -29,18 +31,13 @@ for data in weather_data['list']:
     temp_f = ((temp_k - 273.15) * 1.8) + 32
     formatted_temp_f = '%.1f' % temp_f
     print(formatted_temp_f)
+    tk.Label(window, text=formatted_temp_f).pack()
 
 for hour_data in weather_data['list']:
     condition_code = hour_data['weather'][0]['id']
     if int(condition_code) != 700:
         will_rain = True
-if will_rain:
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    message = client.messages \
-        .create(
-            body='Its gonna rain today. Bring an umbrella',
-            from_='sender',
-            to='your number',
-    )
-    print(message.status)
+        tk.Label(window, text="Its gonna rain!").pack()
 
+
+window.mainloop()
