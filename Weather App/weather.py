1d2578c634ac0dc21c5a216e05c137300d9b7fd3
amanda16229 <amanda.olyer@gmail.com>
2025-08-25 17:30:35 -0400

    allows zip code entry for i/o weather using tkinter gui

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -5,39 +5,60 @@ import requests
 API_KEY = '<api_key>'
 ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
 
-window = tk.Tk()
-window.title('Weather Alert App')
-tk.Label(window, text="Tampa FL weather: ").pack() # using tampa latitude & longitude
+def get_entry(event):
+    zip_code = search_var.get()
+    print("Zip code: ", zip_code)
+
+    # dictionary - must follow the keys provided in API docs
+    weather_params = {
+        "zip": zip_code,
+        "appid": API_KEY,
+        "cnt": 4,  # (optional) - only uses next 4 entries of weather data
+    }
+
+    # want to make our GET requests to the endpoint - check the data they already have, get it
+    # provide params = want type of data we get back (in this case its weather info)
+    response = requests.get(ENDPOINT, params=weather_params) # request sent to api (returns number code)
+    weather_data = response.json()
+    print(weather_data)
+
+    tk.Label(window, text=weather_data['list'][0]['main']).pack() # TODO: hard to read list of weather data, json format
+
+    is_raining(weather_data)
 
 
-# dictionary - must follow the keys provided in API docs
-weather_params = {
-    "lat": 27.9,
-    "lon": 82.4,
-    "appid": API_KEY,
-    "cnt": 4, # (optional) - only uses next 4 entries of weather data
-}
+def is_raining(weather_data):
+    will_rain = False
 
-# want to make our GET requests to the endpoint - check the data they already have, get it
-# provide params = want type of data we get back (in this case its weather info)
-response = requests.get(ENDPOINT, params=weather_params)
+    for data in weather_data['list']:
+        temp_k = data['main']['feels_like']
+        temp_f = ((temp_k - 273.15) * 1.8) + 32
+        formatted_temp_f = '%.1f' % temp_f
+        print(formatted_temp_f)
+        tk.Label(window, text=formatted_temp_f).pack()
 
-weather_data = response.json()
-
-will_rain = False
-
-for data in weather_data['list']:
-    temp_k = data['main']['feels_like']
-    temp_f = ((temp_k - 273.15) * 1.8) + 32
-    formatted_temp_f = '%.1f' % temp_f
-    print(formatted_temp_f)
-    tk.Label(window, text=formatted_temp_f).pack()
-
-for hour_data in weather_data['list']:
-    condition_code = hour_data['weather'][0]['id']
+    for hour_data in weather_data['list']:
+        condition_code = hour_data['weather'][0]['id']
     if int(condition_code) != 700:
         will_rain = True
         tk.Label(window, text="Its gonna rain!").pack()
+
+
+window = tk.Tk()
+window.title('Weather Alert App')
+
+tk.Label(window, text="Search a city for its weather (enter zip code): ").pack() # using tampa latitude & longitude
+
+search_var = tk.StringVar() # holds value of string we will be searching for
+
+search_entry = tk.Entry(window, textvariable=search_var) # adds tkinter search box, grabs entered text & attaches to var
+search_entry.pack()
+
+button = tk.Button(window, text="Search")
+button.pack()
+
+search_entry.bind('<Return>', get_entry)
+
 
 
 window.mainloop()
